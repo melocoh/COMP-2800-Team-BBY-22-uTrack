567dbc5b5a4e2a391c8194c33e4350ed634dc1e4
Kent Claudio <rikysmile33@gmail.com>
2020-04-28 13:08:40 -0700

    Added console logs to login_check.js for testing purposes and implemented the script to index.html and login.html along with a few comments for template

--- scripts/login_check.js
+++ scripts/login_check.js
@@ -3,8 +3,11 @@
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in.
+        console.log("User is signed in.");
     } else {
         // No user is signed in.
+        console.log("User is not signed in.");
         location.href = './login.html';
+        console.log("Page should be re-directed by now.");
     }
 });
