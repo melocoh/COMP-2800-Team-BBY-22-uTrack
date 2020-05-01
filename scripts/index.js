3b860b61dc2a23271382cb23a2bbb6c691752029
Steven Kim <stevenkim1989@gmail.com>
2020-05-01 01:49:03 -0700

    edited typo - users to user

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 let greeting = document.getElementById("username");
 
 // Display greeting if signed in.
-firebase.auth().onAuthStateChanged(function (users) {
-    if (users) {
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
         greeting.innerHTML = "Hello, " + user.displayName;
     }
 })
