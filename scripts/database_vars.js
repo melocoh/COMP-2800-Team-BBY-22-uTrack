1e9367cf92b202f8c07672821555c4f62689f41d
Kent Claudio <rikysmile33@gmail.com>
2020-04-30 22:25:10 -0700

    Created Database Collection Variables
    - Created "database_vars.js" and added variables to store database collection references

---
+++ scripts/database_vars.js
@@ -0,0 +1,9 @@
+/* This file contains variables for referencing database collections
+    and documents */
+/* These variables can be referenced in other JavaScript files */
+
+// Collection Variables
+var colUser = db.collection("users");
+var colItems = db.collection("items");
+var colPosts = db.collection("posts");
+var colStores = db.collection("stores");
