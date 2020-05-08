b04261bb1c38a836fa98b5e8f1135dc1606da53d
Steven Kim <stevenkim1989@gmail.com>
2020-05-07 23:49:49 -0700

    profile styling is in progress

--- scripts/home.js
+++ scripts/home.js
@@ -10,3 +10,16 @@ firebase.auth().onAuthStateChanged(function (user) {
         greeting.innerHTML = "Welcome!";
     }
 })
+
+let nameOnly = document.getElementById("usernameOnly");
+
+// Display greeting if signed in.
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        nameOnly.innerHTML = user.displayName;
+    }
+    // display Welcome if user signed in with phone number ( => user name is null)
+    else {
+        nameOnly.innerHTML = "Please log-in";
+    }
+})
--- scripts/profile.js
+++ scripts/profile.js
@@ -2,8 +2,10 @@ firebase.auth().onAuthStateChanged(function (user) {
     document.querySelector("#userName").innerHTML = user.displayName;
 
     db.collection("/users/").doc(user.uid).onSnapshot(function (snap){
-        document.querySelector("#lv").innerHTML = snap.data().level;
-        document.querySelector("#point").innerHTML = snap.data().points;
+        document.querySelector(".displayLevel").innerHTML = snap.data().level;
+        document.querySelector(".displayPosting").innerHTML = snap.data().post;
+        document.querySelector(".displayPoint").innerHTML = snap.data().points;
+        document.querySelector(".email").innerHTML = snap.data().email;
 
         // let sizeArray = snap.data().user_posts;
         // console.log(sizeArray);
