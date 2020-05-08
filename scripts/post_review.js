c1b35bfc4cd0b774c42c25123d142820931c363a
Thi Hong Phuc Le <plth1998@gmail.com>
2020-05-07 20:19:36 -0700

    modify post_review.js

--- scripts/post_review.js
+++ scripts/post_review.js
@@ -12,11 +12,11 @@ db.collection("posts").get().then(function (querySnapshot){
         // p1.setAttribute("id","itemImage");
         // p2.setAttribute("id","storeName");
         // p3.setAttribute("id","address");
-        // p4.setAttribute("id","itemName");
+        p4.setAttribute("id","itemName");
         p1.innerHTML = doc.data().post_name;
         // p2.src = doc.data().post_image;
         var storeInfo = doc.data().post_store;
-        getStoreInfo(storeInfo,p3);
+        getStoreInfo(storeInfo,p3,p4);
         // getItemInfo(p4);
         text.appendChild(p1);
         text.appendChild(p2);
@@ -29,26 +29,46 @@ db.collection("posts").get().then(function (querySnapshot){
 
 function getStoreInfo(storeInfo, p3, p4){
     storeInfo.get().then(function(doc){
-        p3.innerHTML = doc.data().location;
+        p3.innerHTML = doc.get("location");
         let storeItems = doc.data().store_items;
         console.log(storeItems);
-        getItemInfo(storeItems, p4);
+        getItemInfo(storeItems);
+        p4.innerHTML = items;
     })
 }
 
 function getItemInfo(storeItems, p4){
     // storeItems.forEach(function(doc){
-    //     item = item + doc.data().item_name + ": " + doc.data().stock_number + "</br>";
+    //     item = item + doc.get(item_name) + ": " + doc.get(stock_number) + "</br>";
     // })
     // p4.innerHTML = item;
 
     // storeItems.forEach(myFunction);
+   
+    
     for (let i = 0; i < storeItems.length; i++){
+        var name;
+        var stock;
         storeItems[i].get().then(function(doc){
-            items = items + doc.get(item_name) + ": " + doc.get(stock_number) + "</br>";
+            name = doc.get("item_name");
+            stock = doc.get("stock_number");
+            // var list = document.createElement("li");
+            // list.innerHTML = items + name + ": " + stock;
+            items = items + name + ": " + stock + "</br>";
+            
+            console.log(name);
+            console.log(stock);
+            console.log(items);
+            // items = items + doc.get("item_name") + ": " + doc.get("stock_number") + "</br>";
         })
     }
-    p4.innerHTML = items;
+    $("#itemName").html(items);
+    // for (let i = 0; i < name.length; i++){
+    //     console.log(name[i] + ": " + stock[i]);
+    // }
+    
+    
+    
     // function myFunction(item){
     //     item.get().then(function(doc){
     //         items = items + doc.data().item_name + ": " + doc.data().stock_number + "</br>";
