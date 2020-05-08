4a48552f5dcdc527504daafbe4852461bdba5be1
Kent Claudio <rikysmile33@gmail.com>
2020-05-08 02:18:00 -0700

    Updated Map in Home Page
    - Changed script src of "modal_post.js" to "here_maps_modal.js" in "home.html"
    - Changed starting location of map from BCIT to Metrotown in "here_maps_init.js"
    - Modified "here_maps_markers.js" by changing variable locations and visibility as well as adding iteration over valid stores (with lat and long)
    - Created "modal_div.js" which contains a variable that holds the container for the map modal (in case we need to make multiple)

--- scripts/here_maps_markers.js
+++ scripts/here_maps_markers.js
@@ -1,50 +1,119 @@
-var card = document.createElement("div");
-var text_div = document.createElement("div");
-var text = document.createElement("p");
-
 // store collection
-var storeList = db.collection("stores");
+var allStores = db.collection("stores");
+
+// array that holds all stores with valid locations (lat and long)
+var locStores = [];
+
+// current store
+var store;
+
+function getAllStores() {
+    allStores.get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            let hasLoc = doc.get("lat");
+            console.log("hasLoc: " + hasLoc);
+
+            if (hasLoc !== undefined) {
+                console.log(doc);
+                locStores.push(doc);
+            } else {
+                console.log("no lat");
+            }
+        });
+        console.log(locStores);
+        addMarkers(map);
+    });
+
+}
 
 // store document (global var)
-var store = storeList.doc("6ufTjE5tYRWppAXi2l8q");
+// var store = storeList.doc("6ufTjE5tYRWppAXi2l8q");
+
 
 function addMarkers(map) {
-    card.setAttribute("class", "card");
-    // fuuuuuuuuuu
-    card.setAttribute("data-toggle", "modal");
-    card.setAttribute("data-target", "#myModal");
-    text_div.setAttribute("class", "card-body");
-    text.setAttribute("class", "card-text");
+    console.log("inside addMarkers(map)");
+    for (let i = 0; i < locStores.length; i++) {
+        console.log("inside for loop");
+        store = locStores[i];
+        console.log(store);
 
-    // Vanilla DOM Style
-    // Card (Outer div)
-    // card.style.color = "black";
-    // card.style.backgroundColor = "white";
-    // card.style.width = "50px";
-    // card.style.height = "50px";
-    // card.style.textAlign = "center";
-    $(".card").css({
-        "width": "50px",
-        "height": "50px",
-        "text-align": "center"
-    });
+        let card = document.createElement("div");
+        let text_div = document.createElement("div");
+        let text = document.createElement("p");
 
-    // var store = db.collection("stores").doc("6ufTjE5tYRWppAXi2l8q");
+        card.setAttribute("class", "card");
+        card.setAttribute("data-toggle", "modal");
+        card.setAttribute("data-target", "#myModal");
+        text_div.setAttribute("class", "card-body");
+        text.setAttribute("class", "card-text");
 
-    // test doc id: 6ufTjE5tYRWppAXi2l8q
-    // get store name
-    store.get().then(function (doc) {
-        $(".card-text").text(doc.get("store_name"));
-    });
-    // Why are we here... just to suffer?
+        // Vanilla DOM Style
+        // Card (Outer div)
+        // card.style.color = "black";
+        // card.style.backgroundColor = "white";
+        // card.style.width = "50px";
+        // card.style.height = "50px";
+        // card.style.textAlign = "center";
+        $(".card").css({
+            "width": "50px",
+            "height": "50px",
+            "text-align": "center"
+        });
 
-    // Appendingssssss
-    card.appendChild(text_div);
-    // card.append(postModal);
-    text_div.appendChild(text);
+        // test doc id: 6ufTjE5tYRWppAXi2l8q
+        // get store name
+        // store.get().then(function (doc) {
+        //     $(".card-text").text(doc.get("store_name"));
+        // });
 
-    function initReadLatest() {
-        readLatest();
+        console.log(store.get("store_name"));
+        let store_name = store.get("store_name");
+        $(".card-text").text(store_name);
+
+        // Appendingssssss
+        text_div.appendChild(text);
+        card.appendChild(text_div);
+        // card.append(postModal);
+
+        initReadModal(store);
+
+        // read the latest post from the current store
+        // function initReadLatest(store) {
+        //     readLatest(store);
+        // };
+
+        function readModal() {
+            readLatest();
+        };
+
+        // DOM Icon (needed for second parameter argument of H.map.DomMarker())
+        let domIcon = new H.map.DomIcon(card, {
+            onAttach: function (clonedElement, domIcon, domMarker) {
+                clonedElement.addEventListener("click", readModal());
+            }
+            // onDetach: function (clonedElement, domIcon, domMarker) {
+            //     clonedElement.removeEventListener("click", initReadLatest);
+            // }
+        });
+
+        let storeLat = store.get("lat");
+        let storeLng = store.get("lng");
+
+        // DomMarker par args:
+        //  1st: coordinates
+        //  2nd: DOM Icon
+        let domMarker = new H.map.DomMarker({
+            // lat: 49.249394,
+            // lng: -123.000788
+            lat: storeLat,
+            lng: storeLng
+        }, {
+            icon: domIcon
+        });
+
+        console.log("adding marker...");
+        // add DOM Marker to map
+        map.addObject(domMarker);
     }
 
     // function closeModal(event) {
@@ -59,45 +128,11 @@ function addMarkers(map) {
     // function changeOpacityToOne(event) {
     //     event.target.style.opacity = 1;
     // };
-
-    // DOM Icon (needed for second parameter argument of H.map.DomMarker())
-    var domIcon = new H.map.DomIcon(card, {
-        onAttach: function (clonedElement, domIcon, domMarker) {
-            clonedElement.addEventListener("click", initReadLatest);
-        }
-        // onDetach: function (clonedElement, domIcon, domMarker) {
-        //     clonedElement.removeEventListener("click", initReadLatest);
-        // }
-    });
-    // , {
-    //     onAttach: function (clonedElement, domIcon, domMarker) {
-    //         // clonedElement.addEventListener("click", openModal);
-    //         // clonedElement.addEventListener("mouseover", changeOpacity);
-    //         // clonedElement.addEventListener("mouseout", changeOpacityToOne);
-    //     },
-    //     onDetach: function (clonedElement, domIcon, domMarker) {
-    //         // clonedElement.removeEventListener("click", openModal);
-    //         // clonedElement.removeEventListener("mouseover", changeOpacity);
-    //         // clonedElement.removeEventListener("mouseout", changeOpacityToOne);
-    //     }
-    // }
-
-    // DomMarker par args:"
-    //  1st: coordinates
-    //  2nd: DOM Icon
-    var domMarker = new H.map.DomMarker({
-        lat: 49.249394,
-        lng: -123.000788
-    }, {
-        icon: domIcon
-    });
-
-    // add DOM Marker to map
-    map.addObject(domMarker);
 }
 
 $(document).ready(function () {
-    addMarkers(map);
+    getAllStores();
+    // addMarkers(map);
 })
 
 // addMarkers(map);
