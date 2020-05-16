3f37645beba013341e30751b48973c38b33420c0
Kent Claudio <rikysmile33@gmail.com>
2020-05-15 21:19:24 -0700

    Revert "Deleted and Moved Files"
    This reverts commit da9eb67e932e59bf1a76fda5b45b350ace1b4f2c.

---
+++ scripts/database_vars.js
@@ -0,0 +1,7 @@
+/* This file contains variables for referencing database collections
+    and documents */
+/* These variables can be referenced in other JavaScript files */
+
+// Collection Variables
+
+console.log("passed");
