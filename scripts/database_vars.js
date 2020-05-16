da9eb67e932e59bf1a76fda5b45b350ace1b4f2c
Kent Claudio <rikysmile33@gmail.com>
2020-05-15 21:18:41 -0700

    Deleted and Moved Files
    - Deleted unused JavaScript files
    - Deleted placeholder text files in populated folders
    - Moved respective HTML files marked as "test" to new "tests" folder (dependencies are NOT updated to reflect the file's new location)
    - Moved unused HTML files to "unused" folder (dependencies are NOT updated to match the file's new location)

--- scripts/database_vars.js
+++
@@ -1,7 +0,0 @@
-/* This file contains variables for referencing database collections
-    and documents */
-/* These variables can be referenced in other JavaScript files */
-
-// Collection Variables
-
-console.log("passed");
